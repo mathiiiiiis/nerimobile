4ce41845854ea40e931f547fa3c43072e730cd46
mathiiiiiis <mathis@mathiiis.de>
2026-09-06 19:17:24 +0200

    feat(chat): cover shell on compact, compose it on expanded

--- lib/views/chat/channel_pane.dart
+++ lib/views/chat/channel_pane.dart
@@ -4,12 +4,36 @@ import 'package:nerimobile/theme/core/theme_data.dart';
 import 'package:nerimobile/theme/core/token.dart';
 import 'package:nerimobile/theme/sizing/breakpoints.dart';
 import 'package:nerimobile/views/chat/channel_header.dart';
+import 'package:nerimobile/views/dashboard/dm_list.dart';
+import 'package:nerimobile/views/shell/app_scaffold.dart';
+import 'package:nerimobile/views/shell/destinations.dart';
 import 'package:nerimobile/views/shell/widgets/panes.dart';
 
 class ChannelPane extends StatelessWidget {
   const ChannelPane({super.key, required this.channelId});
 
   final String channelId;
+
+  @override
+  Widget build(BuildContext context) {
+    final dualPane = NeriWindow.of(context).isDualPane;
+    final chat = _Chat(channelId: channelId, showBack: !dualPane);
+
+    if (!dualPane) return SafeArea(child: chat);
+
+    return AppScaffold(
+      branch: NeriBranch.dashboard,
+      listPane: const DmListPane(),
+      content: chat,
+    );
+  }
+}
+
+class _Chat extends StatelessWidget {
+  const _Chat({required this.channelId, required this.showBack});
+
+  final String channelId;
+  final bool showBack;
 
   @override
   Widget build(BuildContext context) {
