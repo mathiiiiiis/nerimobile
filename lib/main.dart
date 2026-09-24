2ea80ee5f00af5e14b578ed2d54fc9d4cc46a6e1
mathiiiiiis <mathis@mathiiis.de>
2026-09-24 14:33:49 +0200

    perf(emoji): keep the whole emoji set in the svg cache

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:timezone/data/latest_all.dart' as timezones;
 
 import 'package:nerimobile/app.dart';
 
+const _svgCacheSize = 2500;
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  svg.cache.maximumSize = _svgCacheSize;
   MediaKit.ensureInitialized();
   timezones.initializeTimeZones();
   runApp(const ProviderScope(child: MainApp()));
